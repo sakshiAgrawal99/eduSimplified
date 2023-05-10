e4f37f90f470aeda07e551d7c7203142d83e565b
Sakshi Agrawal <sakshi.agrawal@newvisionsoftpune.com>
2023-05-10 16:45:10 +0530

    added modal for send message

--- src/app/admin/student-details/student-details.component.ts
+++ src/app/admin/student-details/student-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-student-details',
   templateUrl: './student-details.component.html',
-  styleUrls: ['./student-details.component.css']
+  styleUrls: ['./student-details.component.css'],
 })
 export class StudentDetailsComponent {
-
+  constructor(protected modalService: ModalService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { StudentDetailsComponent } from './admin/student-details/student-details.component';
+import { ModalComponent } from './admin/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     AdminDashboardComponent,
-    StudentDetailsComponent
+    StudentDetailsComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
