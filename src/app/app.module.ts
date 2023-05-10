907fe51994ce7566b0ef8cd89afd72934c660d3c
Sakshi Agrawal <sakshi.agrawal@newvisionsoftpune.com>
2023-05-10 15:07:41 +0530

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule
