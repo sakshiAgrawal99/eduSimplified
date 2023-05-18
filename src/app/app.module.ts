5d8fa5bb8ef8ba868c87a35f8f98f368df576659
Sakshi Agrawal <sakshi.agrawal@newvisionsoftpune.com>
2023-05-18 16:06:03 +0530

    added dummy header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,15 @@ import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
   { path: 'admin/dashboard', component: AdminDashboardComponent },
   { path: 'admin/student-details', component: StudentDetailsComponent },
   { path: 'landing-page', component: LandingPageComponent },
+  // { path: 'landing-page', component: HeaderComponent },
 ];
 
 @NgModule({
@@ -25,6 +28,8 @@ const routes: Routes = [
     AdminDashboardComponent,
     StudentDetailsComponent,
     ModalComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
