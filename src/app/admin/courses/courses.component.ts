1e4a9176d6930ddaa5ecd1a7eceff31560da0eed
Sakshi Agrawal <sakshi.agrawal@newvisionsoftpune.com>
2023-05-27 21:55:41 +0530

    Integrated get api for cources

--- src/app/admin/courses/courses.component.ts
+++ src/app/admin/courses/courses.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { CoursesService } from 'src/app/services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent {
+  courseData: any[] = [];
 
+  constructor(public coursesService: CoursesService) {
+    this.coursesService.getCourses().subscribe((res) => {
+      this.courseData = res;
+      console.log(this.courseData);
+    });
+  }
+
+  getClass(index: number) {
+    if (index % 10 == 1 || index % 10 == 6) {
+      return 'btn-outline-success';
+    }
+    if (index % 10 == 2 || index % 10 == 7) {
+      return 'btn-outline-secondary';
+    }
+    if (index % 10 == 3 || index % 10 == 8) {
+      return 'btn-outline-warning';
+    }
+    if (index % 10 == 4 || index % 10 == 9) {
+      return 'btn-outline-primary';
+    }
+    if (index % 10 == 5 || index % 10 == 0) {
+      return 'btn-outline-danger';
+    }
+    return 'btn-outline-dark';
+  }
 }
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  bseUrl = 'https://bkbcollegemanagementapi.azure-api.net';
+  constructor(private http: HttpClient) {}
+
+  getCourses() {
+    return this.http.get<any[]>(this.bseUrl + '/api/Courses');
+  }
+}
